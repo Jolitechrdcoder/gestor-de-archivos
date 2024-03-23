01f080649f4918bd72947af395339b4167820d16
Jorge Luis <114448996+Jolitechrdcoder@users.noreply.github.com>
2024-03-23 15:01:02 -0400

    proyecto para youcolor gestor de archivos

---
+++ sammy/users.php
@@ -0,0 +1,75 @@
+<?php 
+
+?>
+
+<div class="container-fluid">
+	
+	<div class="row">
+	<div class="col-lg-12">
+			<button class="btn btn-primary float-right btn-sm" id="new_user"><i class="fa fa-plus"></i> New user</button>
+	</div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="card col-lg-12">
+			<div class="card-body">
+				<table class="table-striped table-bordered col-md-12">
+			<thead>
+				<tr>
+					<th class="text-center">#</th>
+					<th class="text-center">Nombre</th>
+					<th class="text-center">Usuario</th>
+					<th class="text-center">Acción</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+ 					include 'db_connect.php';
+ 					$users = $conn->query("SELECT * FROM users order by name asc");
+ 					$i = 1;
+ 					while($row= $users->fetch_assoc()):
+				 ?>
+				 <tr>
+				 	<td>
+				 		<?php echo $i++ ?>
+				 	</td>
+				 	<td>
+				 		<?php echo $row['name'] ?>
+				 	</td>
+				 	<td>
+				 		<?php echo $row['username'] ?>
+				 	</td>
+				 	<td>
+				 		<center>
+								<div class="btn-group">
+								  <button type="button" class="btn btn-primary">Acción</button>
+								  <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								    <span class="sr-only">Alternar menú desplegable</span>
+								  </button>
+								  <div class="dropdown-menu">
+								    <a class="dropdown-item edit_user" href="javascript:void(0)" data-id = '<?php echo $row['id'] ?>'>Editar</a>
+								    <div class="dropdown-divider"></div>
+								    <a class="dropdown-item delete_user" href="javascript:void(0)" data-id = '<?php echo $row['id'] ?>'>Eliminar</a>
+								  </div>
+								</div>
+								</center>
+				 	</td>
+				 </tr>
+				<?php endwhile; ?>
+			</tbody>
+		</table>
+			</div>
+		</div>
+	</div>
+
+</div>
+<script>
+	
+$('#new_user').click(function(){
+	uni_modal('Nuevo Usuario','manage_user.php')
+})
+$('.edit_user').click(function(){
+	uni_modal('Edit User','manage_user.php?id='+$(this).attr('data-id'))
+})
+
+</script>
